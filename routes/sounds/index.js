5253c84986485425e7ae801ca2ad90e8839575a8
Eddie Reeder <e.reeder1997@gmail.com>
2019-12-07 17:46:33 +0000

    Removed directions from the server

--- routes/sounds/index.js
+++ routes/sounds/index.js
@@ -3,7 +3,6 @@ const router = express.Router();
 
 const db = require("../../config/db");
 const isAuthenticated = require("../../middlewares/isAuthenticated");
-const generateDirection = require("./utils/generateDirection");
 
 router.get("/", async (req, res, next) => {
   try {
@@ -12,9 +11,6 @@ router.get("/", async (req, res, next) => {
       sounds: rows.map(sound => {
         return {
           id: sound.id,
-          directionX: sound.direction_x,
-          directionY: sound.direction_y,
-          directionZ: sound.direction_z,
           location: sound.location,
           description: sound.description,
           category: sound.category,
@@ -38,9 +34,6 @@ router.get("/selected", async (req, res, next) => {
       sounds: rows.map(sound => {
         return {
           id: sound.id,
-          directionX: sound.direction_x,
-          directionY: sound.direction_y,
-          directionZ: sound.direction_z,
           location: sound.location,
           description: sound.description,
           category: sound.category,
@@ -76,9 +69,9 @@ router.post("/", isAuthenticated, async (req, res, next) => {
     const { rows } = await db.query(
       `
       INSERT INTO sounds
-      (direction_x, direction_y, direction_z, location, description, category, cd_number, cd_name, track_number, selected)
+      (location, description, category, cd_number, cd_name, track_number, selected)
       VALUES
-      (null, null, null, $1, $2, $3, $4, $5, $6, False)
+      ($1, $2, $3, $4, $5, $6, False)
     `,
       [
         req.body.location,
@@ -117,16 +110,14 @@ router.post("/:id/select", isAuthenticated, async (req, res, next) => {
     return res.status(400).json({ message: "Invalid parameters" });
   }
   try {
-    // Generate direction for the sound
-    const direction = await generateDirection();
     // Attempt to update
     const { rows } = await db.query(
       `
       UPDATE sounds 
-      SET (selected, direction_x, direction_y, direction_z) = (True, $1, $2, $3)
-      WHERE id=$4
+      SET (selected) = (True)
+      WHERE id=$1
     `,
-      [direction.x, direction.y, direction.z, req.params.id]
+      [req.params.id]
     );
     return res.sendStatus(200);
   } catch (e) {
@@ -144,7 +135,7 @@ router.post("/:id/deselect", isAuthenticated, async (req, res, next) => {
     const { rows } = await db.query(
       `
       UPDATE sounds 
-      SET (selected, direction_x, direction_y, direction_z) = (False, null, null, null)
+      SET (selected) = (False)
       WHERE id=$1
     `,
       [req.params.id]
@@ -154,44 +145,5 @@ router.post("/:id/deselect", isAuthenticated, async (req, res, next) => {
     return next(e);
   }
 });
-
-router.post(
-  "/regenerateDirections",
-  isAuthenticated,
-  async (req, res, next) => {
-    try {
-      // Remove direction from all sounds
-      await db.query(
-        `
-        UPDATE sounds
-        SET (direction_x, direction_y, direction_z) = (null, null, null)
-      `
-      );
-      // Retrieve all selected sounds
-      const { rows } = await db.query(
-        `
-        SELECT *
-        FROM sounds
-        WHERE selected=True
-      `
-      );
-      // Generate a new direction for each sound and update it
-      for (row of rows) {
-        const direction = await generateDirection();
-        await db.query(
-          `
-          UPDATE sounds
-          SET (direction_x, direction_y, direction_z) = ($1, $2, $3)
-          WHERE id=$4
-        `,
-          [direction.x, direction.y, direction.z, row.id]
-        );
-      }
-      return res.sendStatus(200);
-    } catch (e) {
-      return next(e);
-    }
-  }
-);
 
 module.exports = router;
